22768f071f3797bd120028a007365577e0452b7e
Bipin Singh <b.singh@dsslp.com>
2022-02-13 16:22:57 +0530

    pdf file creation

--- BackendApi/index.js
+++ BackendApi/index.js
@@ -1,6 +1,9 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+
 var cors = require('cors');
 var app = express();
 var router = express.Router();
@@ -8,7 +11,7 @@ const morgan = require('morgan');
 const _ = require('lodash');
 const fileUpload = require('express-fileupload');
 const transmittalRoutes = require("./routes/transmittalRoutes");
-
+const homeRoutes = require('./routes/home-routes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -16,6 +19,16 @@ app.use(cors());
 //app.use(express.static(path.join(__dirname, 'views')));
 app.use('', router);
 app.use(morgan('dev'));
+
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/docs', express.static(path.join(__dirname, 'docs')));
+
+
+
+
 // enable files upload
 app.use(fileUpload({createParentPath: true,  limits: { 
        fileSize: 2 * 1024 * 1024 * 1024 //2MB max file(s) size
@@ -28,6 +41,7 @@ router.use((request,response,next)=>{
    console.log('middleware');
    next();
 })
+app.use(homeRoutes.routes);
 app.use(transmittalRoutes);
 
 //app.use('/', (req,res, next)=>{
